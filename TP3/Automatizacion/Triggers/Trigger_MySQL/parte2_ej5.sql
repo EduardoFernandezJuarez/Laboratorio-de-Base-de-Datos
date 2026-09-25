5abe588bf39745cd7c96ff594e509087f863a40e
Vicky <victoria.bugli@est.fi.uncoma.edu.ar>
2026-09-25 15:11:21 -0300

    Add procedures for LOG operations

---
+++ TP3/Automatizacion/Triggers/Trigger_MySQL/parte2_ej5.sql
@@ -0,0 +1,28 @@
+DELIMITER $$
+
+CREATE PROCEDURE insertar_recibe(IN p_legajo VARCHAR(20), IN p_id_kit INT)
+BEGIN
+INSERT INTO recibe_esquema_grupo3 (legajo,fecha,id_kit)
+VALUES (p_legajo,CURDATE(),p_id_kit);
+
+INSERT INTO LOG_planillaControl (operacion) VALUES (‘INSERT’);
+END$$
+
+CREATE PROCEDURE actualizar_recibe(IN p_legajo VARCHAR(20))
+BEGIN
+UPDATE recibe_esquema_grupo3 
+SET fecha = CURDATE()
+WHERE legajo = p_legajo;
+
+INSERT INTO LOG_planillaControl (operacion) VALUES (‘UPDATE’);
+END$$
+
+CREATE PROCEDURE eliminar_recibe(IN p_legajo VARCHAR(20))
+BEGIN
+DELETE FROM recibe_esquema_grupo3 
+WHERE legajo = p_legajo;
+
+INSERT INTO LOG_planillaControl (operacion) VALUES (‘DELETE’);
+END$$
+
+DELIMITER;
